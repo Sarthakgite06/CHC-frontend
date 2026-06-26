3964a18524dc42b61a18091d823608fc3719fd18
Sarthakgite06 <sarthakgite006@gmail.com>
2026-06-26 17:50:12 +0530

    Connect frontend to Render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8081',
+  baseURL: 'https://chc-backend-ac0i.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
